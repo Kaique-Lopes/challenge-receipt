1d4836e0109655921b8326ffddcc0c93138d33be
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-03-26 18:16:26 -0300

    create ReceiptListViewModel

---
+++ challenge-receipt/challenge-receipt/ViewController/ReceiptListViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ReceiptListViewController.swift
+//  challenge-receipt
+//
+//  Created by Kaique Lopes de Oliveira on 26/03/24.
+//
+
+import UIKit
+
+class ReceiptListViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private var viewModel: ReceiptListViewModel!
+    private let tableView = UITableView() //tables
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        viewModel = ReceiptListViewModel()
+        viewModel.delegate = self
+        viewModel.fetchReceipts()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupViews() {
+        title = "Comprovantes"
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ReceiptTableViewCell.self, forCellReuseIdentifier: "ReceiptTableViewCell")
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - TableView Delegate & DataSource
+
+extension ReceiptListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfReceipts()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReceiptTableViewCell", for: indexPath) as! ReceiptTableViewCell
+        let receipt = viewModel.receipt(at: indexPath.row)
+        cell.configure(with: receipt)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.didSelectReceipt(at: indexPath.row)
+    }
+}
+
+// MARK: - Receipt List ViewModel Delegate
+
+extension ReceiptListViewController: ReceiptListViewModelDelegate {
+    func reloadData() {
+        tableView.reloadData()
+    }
+    
+    func navigateToDetailScreen(with receipt: Receipt) {
+        let detailVC = ReceiptDetailViewController(receipt: receipt)
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
+
--- challenge-receipt/challenge-receipt/ViewModel/ReceiptListViewModel.swift
+++ challenge-receipt/challenge-receipt/ViewModel/ReceiptListViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+protocol ReceiptListViewModelDelegate: AnyObject {
+    func reloadData()
+    func navigateToDetailScreen(with receipt: Receipt)
+}
+
+class ReceiptListViewModel {
+    
+    weak var delegate: ReceiptListViewModelDelegate?
+    private var receipts: [Receipt] = []
+    
+    func fetchReceipts() {
+        guard let path = Bundle.main.path(forResource: "mocked_receipts", ofType: "json") else {
+            return
+        }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let jsonData = try JSONDecoder().decode(DataObject.self, from: data)
+            receipts = jsonData.data.receipts
+            delegate?.reloadData()
+        } catch {
+            print("Error decoding JSON: \(error)")
+        }
+    }
+    
+    func numberOfReceipts() -> Int {
+        return receipts.count
+    }
+    
+    func receipt(at index: Int) -> Receipt {
+        return receipts[index]
+    }
+    
+    func didSelectReceipt(at index: Int) {
+        let receipt = receipts[index]
+        delegate?.navigateToDetailScreen(with: receipt)
+    }
+}
