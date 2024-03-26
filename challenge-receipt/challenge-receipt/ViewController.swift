0716ba8783809d50a7de65957fc925de2b882b0f
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-03-26 18:00:12 -0300

    Create Project

---
+++ challenge-receipt/challenge-receipt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge-receipt
+//
+//  Created by Kaique Lopes de Oliveira on 26/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
